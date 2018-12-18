62a421630a0c89e26182a31b57bb19c7f951027f
Bilja <biljanalj85@gmail.com>
2018-12-18 02:46:54 +0100

    First assg part 1.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes'
 import './App.css';
+
 
 import { Navbar, NavbarBrand } from 'reactstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes:DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +23,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+
+    render() {
+        return (
+            <div className="row">
+                <Card className="col-12 col-md-5 m-1">
+                  <p></p>
+                </Card>
+                <div className="col-12 col-md-5 m-1">
+                    ffffff
+                </div>
+            </div>
+        );
+    }
+}
+
+export default DishDetail;
