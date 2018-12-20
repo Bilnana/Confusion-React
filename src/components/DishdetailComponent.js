087f59ee170df30d2dcf340097d6c5cf54c9bd57
Biljana Ljubojevic <biljana.ljubojevic@etondigital.com>
2018-12-20 16:55:25 +0100

    Containers and Presentational components.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -22,46 +22,49 @@ class DishDetail extends Component {
         }
     }
 
-    renderComments(comments) {
-        if(comments !=null) {
-            return (
-                this.props.comments.map((comment)=> {
-                    return (
-                        <li key={comment.id}>
-                            <p>{comment.comment}</p>
-                            <p>--{comment.author} , {comment.date}</p>
-                        </li>
-                    )
-                })
-            )
+    // renderComments(props) {
+    //     if(props.comments !=null) {
+    //         return (
+    //             props.comments.map((comment)=> {
+    //                 return (
+    //                     <li key={comment.id}>
+    //                         <p>{comment.comment}</p>
+    //                         <p>--{comment.author} , {comment.date}</p>
+    //                     </li>
+    //                 )
+    //             })
+    //         )
            
-            console.log('NOT null');
+    //         console.log('NOT null');
 
-        } else {
-            return (
-                <div></div>
-            )
+    //     } else {
+    //         return (
+    //             <div></div>
+    //         )
 
-            console.log('null');
-        }
-    }
+    //         console.log('null');
+    //     }
+    //     console.log(props.comments);
+    // }
     
     render() {
        
         //console.log(this.props.selectedComments);
 
         return (
-            <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(this.props.dish)}
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        {this.renderDish(this.props.dish)}
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <ul className="list-unstyled text-left" >
+                            <h4>Comments</h4>
+                            {/* {this.renderComments()} */}
+                        </ul>
+                    </div>
                 </div>
-                <div className="col-12 col-md-5 m-1">
-                    <ul className="list-unstyled text-left" >
-                        <h4>Comments</h4>
-                        {this.renderComments(this.props.comments)}
-                    </ul>
-                </div>
-            </div> 
+            </div>
         );
     }
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import DishDetail from './DishdetailComponent';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-          selectedDish : null
-        };
-    }
-
-    onDishSelect(dish){
-      this.setState({
-        selectedDish : dish,
-        selectedComments : dish.comments 
-      });
     }
 
     renderDish(dish){
@@ -37,8 +26,9 @@ class Menu extends Component {
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
-              <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card onClick = {() => this.onDishSelect(dish)}>
+              <div className="col-12 col-md-5 m-1">
+                  <Card key={dish.id}
+                        onClick={() => this.props.onClick(dish.id)}>
                     <CardImg width="100%" object src={dish.image} alt={dish.name} />
                     <CardImgOverlay>
                       <CardTitle>{dish.name}</CardTitle>
@@ -53,8 +43,6 @@ class Menu extends Component {
             <div className="row">
               {menu}
             </div>
-            <DishDetail dish = {this.state.selectedDish} comments = {this.state.selectedComments}/>
-            
           </div>
         );
     }
